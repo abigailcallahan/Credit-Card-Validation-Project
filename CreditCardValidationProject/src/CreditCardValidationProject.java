8793bb1dd4b6719f8ad74928c6f0357feab7edc1
abigail.callahan <abigail.callahan@MU-C1-02.mullen.pvt.k12.co.us>
2021-10-21 11:40:18 -0600

    all done up to deciding if valid or not

--- CreditCardValidationProject/src/CreditCardValidationProject.java
+++ CreditCardValidationProject/src/CreditCardValidationProject.java
@@ -1,60 +1,121 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class CreditCardValidationProject
 	{
 		//an L on the end tells the computer it is a Long
-		static String creditCardTest = "5424180123456789";
-		static int [] ccArray;
-		static int num;
+		static long creditCardTest;
+		static long creditCardTest1;
+		static long [] ccArray = new long[16];
+		static long digit;
+		static long doubleDigit1;
+		static long doubleDigit2;
+		static long ccSum;
+		
 		
 		//digit strip each number and put it into an int array
-		public static void main(String[] args)
+		public static void main(String[] args) throws IOException
 			{
-				digitStripNum();
-				for(int i = 0; i < ccArray.size(); i++)
+				Scanner myFile = new Scanner(new File("credit.txt"));
+				
+				//while it goes through each token
+				while(myFile.hasNext()) 
 					{
-						System.out.println(ccArray[i]);
+						creditCardTest = myFile.nextLong();
+						creditCardTest1 = myFile.nextLong();
+						intoArray();
+						//printArray();
+						takeAltNums();
+						//printAfterAlt();
+						addDoubleDigits();
+						//printAfterDD();
+						sumAllDigits();
 					}
 			}
-		public static void digitStripNum()
+	
+		public static void intoArray()
 			{
-				//take all odd numbers and put into array
-				for(int i = 0; i < creditCardTest.length(); i+=2)
+				//takes all the numbers and puts into array
+				for(int i = 0; i < ccArray.length; i++)
 					{
-						ccArray[i] = i;
+						digit = creditCardTest1 % 10;
+						ccArray[15 - i] = digit;
+						creditCardTest1 = creditCardTest1 / 10;
 					}
 			}
+		public static void printArray()
+		{
+			System.out.println("Your array is: ");
+			for(int i = 0; i < ccArray.length; i++)
+				{
+					System.out.print(ccArray[i] + " ");
+				}
+		}
+		public static void takeAltNums()
+		{
+			for(int i = 0; i < ccArray.length; i+=2)
+				{
+					ccArray[i] = ccArray[i] * 2;
+				}
+		}
+		public static void printAfterAlt()
+		{
+			System.out.println("\n\nYour new array is: ");
+			for(int i = 0; i < ccArray.length; i++)
+				{
+					System.out.print(ccArray[i] + " ");
+				}
+		}
+		public static void addDoubleDigits()
+		{
+			for(int i = 0; i < ccArray.length; i++)
+				{
+					if(ccArray[i] > 9)
+						{
+							//digit strip it
+							doubleDigit1 = ccArray[i] % 10;
+							doubleDigit2 = ccArray[i] / 10;
+							ccArray[i] = doubleDigit1 + doubleDigit2;
+						}
+				}
+		}
+		public static void printAfterDD()
+		{
+			System.out.println("\n\nYour new array is: ");
+			for(int i = 0; i < ccArray.length; i++)
+				{
+					System.out.print(ccArray[i] + " ");
+				}
+		}
+		public static void sumAllDigits()
+		{
+			ccSum = 0;
+			for(int i = 0; i < ccArray.length; i++)
+				{
+					ccSum = ccSum + ccArray[i]; 
+				}
+			System.out.println("\n\nThe sum is: " + ccSum);
+			
+			if(ccSum % 10 == 0)
+				{
+					System.out.println("The credit card number " + creditCardTest + " is potentially valid.");
+				}
+			else
+				{
+					System.out.println("The credit card number " + creditCardTest + "is not valid.");
+				}
+				
+			
+		}
 		
-			//practice it first with a valid: 
+		//practice it first with a valid: 
 				//5424 1801 2345 6789
 				//change 1 digit and test it then 
 				//after that, import the file
 
 				//import text file, split each line, strip spaces, and put each number into array
 				
-				//long is another primitive because int is too small
-			//long [] creditCardTest = {5, 4, 2, 4, 1, 8, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-			
-//			for(int i = 0; i < creditCardTest.length; i+=2)
-//				{
-//					System.out.print(creditCardTest[i] + " ");
-//				}
-//			System.out.println("\n");
-//			for(int i = 0; i < creditCardTest.length; i+=2)
-//				{
-//					creditCardTest[i] = creditCardTest[i] * 2;
-//					System.out.print(creditCardTest[i] + " ");
-//				}
-//			for(int i = 0; i < creditCardTest.length; i++)
-//				{
-//					//split and add the digits if it is a two digit number
-//					if(creditCardTest[i] > 9)
-//						{
-//							//digit strip it
-//						}
-//				}
-				
-			
-		
-		
 
 	}
